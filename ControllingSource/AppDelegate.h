a4abb56e01bc241a2d99fafe70b364e8daa3451b
y <xk@ytekiMacBook-Air.local>
2014-04-08 20:36:14 +0800

    Initial Commit

---
+++ ControllingSource/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ControllingSource
+//
+//  Created by y on 14-4-8.
+//  Copyright (c) 2014年 xiaokun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ControllingSource/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ControllingSource
+//
+//  Created by y on 14-4-8.
+//  Copyright (c) 2014年 xiaokun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
